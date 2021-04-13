57eda8170743faa96109bdc6b25604a603b9fd4e
Miroux Mathis <mathis.miroux@ensta-paris.fr>
2021-04-13 17:13:00 +0200

    J'ai ajouté def temps de gestation

--- TDD/unittests_Thibaud_Rasse.py
+++ TDD/unittests_Thibaud_Rasse.py
@@ -4,4 +4,8 @@ class testAnimal(unittest.TestCase):
 
 	def couleurPossible=["bleu","rouge","vert","jaune"]
 
+	def test_temps_gestation(self)
+		self.assertRaises(IN104_TD2.OutOfRangeError, IN104_TD2.main, 9) #créer cdans IN104TD2 un si temps de gestation trp grand raise l'erreur quu'il faut.
+
+
 

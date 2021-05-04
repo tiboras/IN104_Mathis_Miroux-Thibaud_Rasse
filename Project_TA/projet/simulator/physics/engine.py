29f59f2252f3003b6054d7c55f25a6af37c86456
Mathis-302 <81634172+Mathis-302@users.noreply.github.com>
2021-05-04 17:09:21 +0200

    Update engine.py

--- Project_TA/projet/simulator/physics/engine.py
+++ Project_TA/projet/simulator/physics/engine.py
@@ -2,7 +2,7 @@ from ..utils.vector import Vector, Vector2
 from .constants import G
 import numpy as np
 
-
+#à intégrer qqpart dans le code
 def gravitational_force(pos1, mass1, pos2, mass2):
     """ Return the force applied to a body in pos1 with mass1
         by a body in pos2 with mass2
@@ -16,7 +16,7 @@ class IEngine:
     def __init__(self, world):
         self.world = world
 
-    def derivatives(self, t0, y0, masses,N):
+    def derivatives(self, t0, y0, ,N):
         """ This is the method that will be fed to the solver
             it does not use it's first argument t0,
             its second argument y0 is a vector containing the positions 
@@ -29,22 +29,28 @@ class IEngine:
             where vxi, vyi are the velocities and axi, ayi are the accelerations.
         """
         """faire une matrice des forces"""
-        forces = np.empty((N,N), dtype = Vector) #utiliser le inti ?
+        N = len(world)
+        Fx = np.empty((N,N), dtype = Vector) #utiliser le inti ?
+        Fy = np.empty((N,N), dtype = Vector) #utiliser le inti ?
         for i in range(1,len(world)+1):
             for j in range(1,i):
-                F[i][j]=G*mass.get.world(i)*mass.get.world(j)*(position.get.world(i)-position.get.world(j))/norm.Vector(position.get.world(i)-position.get.world(j))**(3/2)
-                F[j,i] = -F[i,j]
-        forces_appliques = forces * np.ones((N,1)) #faire la somme des forces appliqueés à toutes les planètes
+                Fx[i][j]=G*mass.get.world(i)*mass.get.world(j)*(get_x(position.get.world(i)-position.get.world(j)))/norm.Vector(position.get.world(i)-position.get.world(j))**(3/2)
+                Fx[j,i] = -Fx[i,j]
+                Fy[i][j]=G*mass.get.world(i)*mass.get.world(j)*(get_y(position.get.world(i)-position.get.world(j)))/norm.Vector(position.get.world(i)-position.get.world(j))**(3/2)
+                Fy[j,i] = -Fy[i,j]
+
+
+        forces_appliques_x = Fx * np.ones((N,1)) #faire la somme des forces appliqueés à toutes les planètes
+        forces_appliques_y = Fy * np.ones((N,1)) #faire la somme des forces appliqueés à toutes les planètes
+
         deriv = vector(4*N)
         deriv[1:2*N] = y0[2*N:]
         for i in range(N):
-            deriv[2*N+ (2*i)] = get_x(forces_appliques[i])
-            deriv[2*N+ (2*i+1)] = get_y(forces_appliques[i])
+            deriv[2*N+ (2*i)] = (forces_appliques_x[i])/mass.get.world(i)
+            deriv[2*N+ (2*i+1)] = (forces_appliques_y[i])/mass.get.world(i)
         return deriv
 
-
-        raise NotImplementedError
-
+        
     def make_solver_state(self):
         """ Returns the state given to the solver, it is the vector y in
                 y' = f(t, y)
